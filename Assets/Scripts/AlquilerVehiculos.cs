39a8392bada08bfa8a57aa71915e753a996b687e
NicolasEsquinas <esquinasnico@gmail.com>
2024-05-31 09:03:49 -0300

    Arrays prueba

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            for (int i = 0; i < vehiculos.Length; i++)
+            {
+
+            }
+        }
+    }
+
+    void ResetearVehiculos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+
+    void DesactivarTodosLosElementos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+        
+    }
+    
+    
+        
+
+        
+    
+}
